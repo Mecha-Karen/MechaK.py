88ade5c1c334809b23e229acdf4e65acb6b65cfe
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-03-20 18:07:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
   author_email='smelted02@gmail.com',
   license='Apache', 
   classifiers=classifiers,
-  keywords='API,Wrapper', 
+  keywords='Data-Types,Python', 
   packages=find_packages(),
   install_requires=[''] 
 )
